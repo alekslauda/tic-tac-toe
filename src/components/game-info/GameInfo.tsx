88c98a6d2736777f2fcd495b5b3d14987b272dd0
Aleksander Aleksandrov <alekslauda05@gmail.com>
2022-02-26 13:09:13 +0200

    styling

--- src/components/game-info/GameInfo.tsx
+++ src/components/game-info/GameInfo.tsx
@@ -1,4 +1,5 @@
 import { Game, GameState } from "../../constants/app-constants";
+import './GameInfo.css';
 
 interface GameInfoProps {
   turn: Game.PLAYER1 | Game.PLAYER2,
@@ -10,27 +11,31 @@ interface GameInfoProps {
 
 export default function GameInfo({ turn, gameState, marker, resetGame, nextMove }: GameInfoProps) {
 
+  const outcomeInlineStyle = function(color: string) {
+    return {color: color, fontSize: 15, textDecoration: "underline", textUnderlinePosition: "under"}
+  }
+
   const templateFactory = function (): any[] {
     let template = [];
 
     let resetGamePossibility = false;
     if (gameState === GameState.GAME_ON) {
       template.push(
-        (<li key={`'game_on_1_${ new Date().getTime() }`}>First plays {turn}</li>), 
-        (<li key={`'game_on_2_${ new Date().getTime() }`}>{Game.PLAYER1} plays with: {marker[0]}</li>), 
-        (<li key={`'game_on_3_${ new Date().getTime() }`}>{Game.PLAYER2} plays with: {marker[1]}</li>)
+        (<li key={`'game_on_1_${ new Date().getTime() }`}>First plays: <strong>{turn}</strong></li>), 
+        (<li key={`'game_on_2_${ new Date().getTime() }`}>{Game.PLAYER1} plays with: <strong>{marker[0]}</strong></li>), 
+        (<li key={`'game_on_3_${ new Date().getTime() }`}>{Game.PLAYER2} plays with: <strong>{marker[1]}</strong></li>)
       );
     } else if(gameState === GameState.DRAW) {
       resetGamePossibility = true;
       template.push(
         (<li key={`'game_draw_1_${ new Date().getTime() }`}>The End! Game is draw</li>),
-        (<li key={`'game_draw_2_${ new Date().getTime() }`}>Nobody wins! <strong>:X :X :X</strong></li>)
+        (<li style={outcomeInlineStyle('orange')} key={`'game_draw_2_${ new Date().getTime() }`}>Nobody wins! <strong>:X :X :X</strong></li>)
       )
     } else if(gameState === GameState.WIN) {
       resetGamePossibility = true;
       template.push(
         (<li key={`'game_win_1_${ new Date().getTime() }`}>We have a winner!</li>), 
-        (<li key={`'game_win_2_${ new Date().getTime() }`}>{nextMove} wins the game! <strong>:) :) :)</strong></li>),
+        (<li style={outcomeInlineStyle('green')} key={`'game_win_2_${ new Date().getTime() }`}>{nextMove} wins the game! <strong>:) :) :)</strong></li>),
       )
     } else if(gameState === GameState.NOT_STARTED) {
       resetGamePossibility = true;
